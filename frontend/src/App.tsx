e2804938c0b5e327ddca3543557dcf7a27d462aa
Pedro Lauria <pedroclauria@gail.com>
2022-11-16 14:06:45 -0300

    firt component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import NotificationButton from "./Components/NotificationButton"
 
 
 function App() {
 
   return (
+    <>
     <h1>Hello World! </h1>
+    <NotificationButton />
+    </>
   )
  
 }
