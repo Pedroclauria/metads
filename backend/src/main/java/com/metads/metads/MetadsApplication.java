2e82f1e7f89af954b8d796da7fa20de5c6fb59b6
Pedro Lauria <pedroclauria@gail.com>
2022-11-16 13:38:39 -0300

    Project created

---
+++ backend/src/main/java/com/metads/metads/MetadsApplication.java
@@ -0,0 +1,13 @@
+package com.metads.metads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/metads/metads/MetadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metads.metads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
